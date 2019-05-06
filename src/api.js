57e5e38ca8cd50a3c7af0d577b445ff0ea7dacc0
Александр Мангир <sasha982025@gmail.com>
2019-05-06 03:24:44 +0300

    add 2 files

---
+++ src/api.js
@@ -0,0 +1,8 @@
+﻿import axios from "axios";
+
+export async function getData() {
+    const URL = `http://hp-api.herokuapp.com/api/characters/students`
+    const res = await axios(URL)
+
+    return res
+}
---
+++ src/loader.js
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+import { RingLoader } from 'react-spinners';
+
+export const loadings = (isLoading) => {
+    return (
+        <div className='ring-loader'>
+            <RingLoader
+                sizeUnit={"px"}
+                size={150}
+                color={'#123abc'}
+                loading={isLoading}
+            />
+        </div>
+    )
+}
